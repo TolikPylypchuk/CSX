531343245f5a20c2da2d6b61723a27a8abed232a
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-12-13 21:30:23 +0200

    Add tests for EmptyEnumerable

---
+++ CSX.Tests/Collections/EmptyEnumerableTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+using Xunit;
+
+namespace CSX.Collections
+{
+	public class EmptyEnumerableTests
+	{
+		[Fact(DisplayName = "GetEnumerator returns EmptyEnumerator")]
+		public void TestGetEnumerator()
+		{
+			var enumerator = EmptyEnumerable<int>.Instance.GetEnumerator();
+
+			Assert.True(enumerator is EmptyEnumerator<int>);
+		}
+
+		[Fact(DisplayName = "IEnumerable.GetEnumerator returns EmptyEnumerator")]
+		public void TestGetEnumeratorNonGeneric()
+		{
+			var enumerator = ((IEnumerable)EmptyEnumerable<int>.Instance).GetEnumerator();
+
+			Assert.True(enumerator is EmptyEnumerator<int>);
+		}
+	}
+}
